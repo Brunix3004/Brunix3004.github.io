703d6e08a62d3489d06671f0a062d2c2e2fef0e7
Alessandro Medina <107444607+Brunix3004@users.noreply.github.com>
2024-03-01 15:51:59 -0500

    Update componentes.js

--- componentes.js
+++ componentes.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-  const elementos = document.getqueryselectorAll("[href], [src]");
+  const elementos = document.querySelectorAll("[href], [src]");
 
-elementos.forEach(e => {
-    const href = e.getAttribute("href");
-    if(href) e.setAttribute(`.${href}`)
-    else {
-      const src = e.getAttribute("src")
-      e.setAttribute(`.${src}`)
-    }
-})
+  elementos.forEach(e => {
+      const href = e.getAttribute("href");
+      if(href) e.setAttribute("href", `.${href}`)
+      else {
+        const src = e.getAttribute("src")
+        e.setAttribute("src", `.${src}`)
+      }
+  })
 })
