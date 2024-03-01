d2b32d279fc63b0ff42d1be8922ba8c03d467405
Alessandro Medina <107444607+Brunix3004@users.noreply.github.com>
2024-03-01 16:29:30 -0500

    Update componentes.js

--- componentes.js
+++ componentes.js
@@ -3,11 +3,11 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
   elementos.forEach(e => {
       const href = e.getAttribute("href");
-    ìf (href && href.startsWith("https")) return 
+    if (href && href.startsWith("https")) return 
       if(href) e.setAttribute("href", `..${href}`)
       else {
         const src = e.getAttribute("src")
-        ìf (src && src.startsWith("https")) return
+        if (src && src.startsWith("https")) return
         e.setAttribute("src", `..${src}`)
       }
   })
