7ae3c324144d46b8290a8e1f457a8a7e56cbd2cd
Brunix3017 <brunomedinaagnini@gmail.com>
2024-03-06 21:31:37 -0500

    Login

--- js/carrito.js
+++ js/carrito.js
@@ -1,9 +1,9 @@
 const btnCart = document.querySelector('.container-cart-icon')
 const containerCartProducts = document.querySelector('.container-cart-products')
-
+/**
 window.addEventListener('DOMContentLoaded', () => {
     showHTML()
-})
+}) */
 
 btnCart.addEventListener('click', ()=> {
     containerCartProducts.classList.toggle('hidden-cart')
